ff71eb8d0bb3d47c33dc27fe1339f937b9be8889
Andres Correa Casablanca <castarco@coderspirit.xyz>
2021-04-03 01:53:52 +0200

    feat!: remove Safe
    We remove Safe because it's not entirely clear what's better for the
    user, if relying on brands, flavors or tags for that specific case.

--- nominal/src/Safe.ts
+++
@@ -1,8 +0,0 @@
-import { GenericSafe } from './GenericSafe'
-import { TaintSymbolType } from './internal/Symbols'
-
-export type Safe<BaseType, TypeTag extends string | symbol> = GenericSafe<
-  BaseType,
-  TypeTag,
-  TaintSymbolType
->
