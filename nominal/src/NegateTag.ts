0ddd1a182eeadbd763b3a67ccc6d3226ff8fddcd
Andres Correa Casablanca <castarco@coderspirit.xyz>
2021-04-05 23:11:55 +0200

    feat: WithTag behaves better against multiple negations

--- nominal/src/NegateTag.ts
+++ nominal/src/NegateTag.ts
@@ -1,27 +1,35 @@
 import { NegatedTagWrapper, TagWrapper } from './internal/TagUtils'
-import { TagsMarker } from './internal/Markers'
+import { OptionalTagsMarker, TagsMarker } from './internal/Markers'
 
 export type NegateTag<
   BaseType,
   TypeTag extends string | symbol
 > = BaseType extends TagsMarker<infer BaseType0, infer TypeTags0>
-  ? BaseType0 &
-      (TagsMarker<BaseType0, TagWrapper<TypeTag>> extends TagsMarker<
-        BaseType0,
-        TypeTags0
-      >
-        ? Partial<
-            TagsMarker<
-              BaseType0,
-              TypeTags0 extends TagWrapper<TypeTag> & infer RestTypeTags
-                ? RestTypeTags & NegatedTagWrapper<TypeTag>
-                : Omit<TypeTags0, TypeTag> & NegatedTagWrapper<TypeTag>
-            >
-          >
-        : TagsMarker<
-            BaseType0,
-            TypeTags0 extends TagWrapper<TypeTag> & infer RestTypeTags
-              ? RestTypeTags & NegatedTagWrapper<TypeTag>
-              : Omit<TypeTags0, TypeTag> & NegatedTagWrapper<TypeTag>
-          >)
+  ? NegateTagInternals<BaseType0, TypeTags0, TypeTag>
+  : BaseType extends OptionalTagsMarker<infer BaseType0, infer TypeTags0>
+  ? NegateTagInternals<BaseType0, TypeTags0, TypeTag>
   : BaseType & Partial<TagsMarker<BaseType, NegatedTagWrapper<TypeTag>>>
+
+type NegateTagInternals<
+  BaseType0,
+  TypeTags0,
+  TypeTag extends string | symbol
+> = BaseType0 &
+  (TagsMarker<BaseType0, TagWrapper<TypeTag>> extends TagsMarker<
+    BaseType0,
+    TypeTags0
+  >
+    ? Partial<
+        TagsMarker<
+          BaseType0,
+          TypeTags0 extends TagWrapper<TypeTag> & infer RestTypeTags
+            ? RestTypeTags & NegatedTagWrapper<TypeTag>
+            : Omit<TypeTags0, TypeTag> & NegatedTagWrapper<TypeTag>
+        >
+      >
+    : TagsMarker<
+        BaseType0,
+        TypeTags0 extends TagWrapper<TypeTag> & infer RestTypeTags
+          ? RestTypeTags & NegatedTagWrapper<TypeTag>
+          : Omit<TypeTags0, TypeTag> & NegatedTagWrapper<TypeTag>
+      >)
